e097d2a0e46b83527cae37eb7f13753913dc2ce5
carlvdl <Carlvdl@gmail.com>
2019-11-08 22:12:50 +0200

    edit publisher

--- src/app/publisher-edit/publisher-edit.component.ts
+++ src/app/publisher-edit/publisher-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {UserService} from '../services/user.service';
 import {ActivatedRoute, Router} from '@angular/router';
@@ -18,82 +18,115 @@ export class PublisherEditComponent implements OnInit {
   submitted = false;
   loading = false;
   returnUrl: string;
+  publisher: Publisher;
 
   constructor(
     private formBuilder: FormBuilder,
     public publisherService: PublisherService,
     private router: Router,
     private route: ActivatedRoute,
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     console.log('PublisherComponent...');
+
+    this.getPublisher();
 
     this.publisherForm = this.formBuilder.group({
       code: ['', [Validators.required, Validators.minLength(2)]],
       description: ['', [Validators.required, Validators.minLength(3)]]
     });
 
-    this.returnUrl = 'dashboard'
+    // this.returnUrl = 'dashboard'
   }
 
 
+  getPublisher(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.publisherService.getPublisher(id)
+      .subscribe(publisher => {
+        this.publisher = publisher;
+        console.log('publisher-->' + this.publisher);
+        console.log('We got a publisher, so just fucking work..');
+      });
+  }
+
+
+  // getPublisher(): void {
+  //   const id = +this.route.snapshot.paramMap.get('id');
+  //   this.publisherService.getPublisher(id)
+  //     .subscribe(publisher => this.publisher = publisher);
+  // }
+
+  // ngOnInit(): void {
+  //   this.getHero();
+  // }
+  //
+  // getHero(): void {
+  //   const id = +this.route.snapshot.paramMap.get('id');
+  //   this.heroService.getHero(id)
+  //     .subscribe(hero => this.hero = hero);
+  // }
+
   // convenience getter nfor easy access to form fields
-  get f() { return this.publisherForm.controls; }
+  get f() {
+    return this.publisherForm.controls;
+  }
 
 
   onSubmit() {
     console.log('onSubmit');
 
+
     this.submitted = true;
 
     // stop here if form is invalid
-    if (this.publisherForm.invalid) {
-      return;
+    // if (this.publisherForm.invalid) {
+    //   return;
+    // }
+
+
+    let publisherId = this.publisher.id;
+    console.log('publisherId--> ' + publisherId);
+
+    if (publisherId) {
+      console.log('editing...');
+      this.publisherService.updatePublisher(this.publisher)
+        .subscribe(() =>
+          // this.location.back()
+          console.log('Updating...')
+        );
+
+    } else {
+      console.log('adding...');
+      let code = this.publisherForm.value.code;
+      let description = this.publisherForm.value.description;
+
+      this.loading = true;
+
+      let publisher = new Publisher();
+      publisher.code = code;
+      publisher.description = description;
+
+      this.publisherService.addPublisher(publisher)
+        .pipe(first())
+        .subscribe(
+          data => {
+            console.log('debugging data from server, publisher create ');
+            console.log(data);
+            console.log(this.returnUrl);
+            this.router.navigate([this.returnUrl]);
+          },
+          error => {
+            // this.alertService.error(error);
+            console.log('Use an alert service for the error:');
+            console.log(error);
+            this.loading = false;
+          });
     }
 
-    const user = new User();
-    let code = this.publisherForm.value.code;
-    let description = this.publisherForm.value.description;
 
-    this.loading = true;
-
-    let publisher = new Publisher();
-    publisher.code = code;
-    publisher.description = description;
-
-    this.publisherService.addPublisher(publisher)
-      .pipe(first())
-      .subscribe(
-        data => {
-          console.log('debugging data from server, publisher create ');
-          console.log(data);
-          console.log(this.returnUrl);
-          this.router.navigate([this.returnUrl]);
-        },
-        error => {
-          // this.alertService.error(error);
-          console.log('Use an alert service for the error:')
-          console.log(error)
-          this.loading = false;
-        });
-
-    //
-    // this.publisherService.login(this.loginForm.value.email, this.loginForm.value.password)
-    //   .pipe(first())
-    //   .subscribe(
-    //     data => {
-    //       console.log('debugging data from server ');
-    //       console.log(data);
-    //       console.log(this.returnUrl);
-    //       this.router.navigate([this.returnUrl]);
-    //     },
-    //     error => {
-    //       // this.alertService.error(error);
-    //       console.log('Use an alert service for the error:')
-    //       console.log(error)
-    //       this.loading = false;
-    //     });
   }
 
 }
--- src/app/services/publisher.service.ts
+++ src/app/services/publisher.service.ts
@@ -14,6 +14,18 @@ export class PublisherService {
   constructor(private http: HttpClient) {
   }
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+
+  updatePublisher(publisher: Publisher) : Observable<Publisher> {
+      let url = 'http://localhost:5000/publishers/'+(publisher.id).toString();
+      return this.http.put(url, publisher, this.httpOptions).pipe(
+        tap(_ => this.log(`updated publisher id=${publisher.id}`)),
+        catchError(this.handleError<any>('updatePublisher'))
+      );
+  }
 
   addPublisher(publisherInput: Publisher) {
     let code = publisherInput.code;
@@ -49,6 +61,35 @@ export class PublisherService {
       });
   }
 
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    console.log('message: '+message);
+  }
+
   //edit publisher
+  getPublisher(id: number): Observable<Publisher> {
+    console.log('gettng Publisher by id ');
+    console.log(id);
+    const url = 'http://localhost:5000/publishers/'+id;
+    console.log(url);
+
+    return this.http.get<Publisher>(url).pipe(
+      tap(_ => this.log ('fetched publisher id=${id}')),
+      catchError(this.handleError<Publisher>('getPublisher id=${id}'))
+    );
+  }
 
 }
